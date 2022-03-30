fd8bd1af4417ef1d80b35a35ffb34c0b89b0bf3e
mejarisuresh <100571335+mejarisuresh@users.noreply.github.com>
2022-03-29 21:26:34 -0700

    Update hay.groovy

--- vars/hay.groovy
+++ vars/hay.groovy
@@ -1,5 +1,5 @@
 def call(ip,user,credid){
-                      sshagent(['credid']) {
+                      sshagent(['aaaa']) {
     // some block
        sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/opt/tomcat8/webapps"
      sh "ssh ${user}@${ip} /opt/tomcat8/bin/startup.sh"
